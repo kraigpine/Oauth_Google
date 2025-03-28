9a430f3fc902af213389052d336bffae18a3c758
kraigpine <kraigpine@gmail.com>
2025-03-27 21:43:57 -0500

    Added the rest of the files for knightone theme

--- auth/oauth-google-basic-login.py
+++ auth/oauth-google-basic-login.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify, session
+from flask import Flask, request, redirect, jsonify, session
 from google.oauth2 import id_token
 from google.auth.transport import requests
 import json
@@ -8,7 +8,7 @@ app = Flask(__name__)
 CORS(app)
 
 app.secret_key = 'Do not share this with anyone' #very important to change this.
-CLIENT_ID = 'YOUR CLIENT ID'
+CLIENT_ID = ''
 
   
 @app.route('/process-auth-google', methods=['POST'])
@@ -35,7 +35,7 @@ def process_auth():
         
         # create user if they don't exist
         
-        session['user_id'] = user['id'] #or generate a jwt.
+        # session['user_id'] = user['id'] #or generate a jwt. 
 
         return redirect("http://localhost:8000/dashboard.html") # Replace with your desired URL.
 
